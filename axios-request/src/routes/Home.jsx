a91cece7a05529d22c20b3d5307239f7a557e829
equirinho <erickinacio73@gmail.com>
2023-02-15 12:27:59 -0300

    second commit || i made the request for axios, catching the json webtoken api

--- axios-request/src/routes/Home.jsx
+++ axios-request/src/routes/Home.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'; //this link makes the "single page application", we dont't need to reaload the page when we click on any link
+import "./style-routes/Home.css";
+
+
 
 const Home = () => {
+
+    const [posts,setPosts] = useState([])
+    const getPosts = async() =>{
+        
+        try {
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/posts'
+            );
+
+            const data = response.data //i want the api data, so i transfer the response api for my variable data
+            console.log(data);
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
+
+    useEffect(() =>{ //here i can coordinate the exec function as many times as I want
+
+        
+
+    }, [])
+
+
+
     return <div>Home</div>
 }
 
